8e2e47d2bbdc6f562a61c687316f9f4d0abb5c6b
Omid Zaheri <omid.zaheri380@gmail.com>
2024-07-06 19:24:03 +0330

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Omid Zaheri on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
